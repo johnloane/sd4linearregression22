c37056ab6ed4e25a1678c43a577f0860e71214ee
John Loane <john.loane@dkit.ie>
2021-10-19 22:11:28 +0100

    Fix for problem that we had in class 19-10-21
    There was nothing major wrong with the code. The problem was with
    the learning rate that we were using. I think it was 2 and this
    was way too high. I have changed it to 0.002 in this code and only
    drawn the line if the x2 values don't get out of hand

--- lr.py
+++ lr.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 def draw(x1, x2):
     ln = plt.plot(x1, x2)
+    plt.pause(0.001)
+    ln[0].remove()
 
 
 def sigmoid(score):
@@ -18,6 +20,25 @@ def calculate_error(line_parameters, points, labels):
     return cross_entropy
 
 
+def gradient_descent(line_parameters, points, labels, learning_rate):
+    m = points.shape[0]
+    #print(line_parameters)
+    for i in range(20000):
+        p = sigmoid(np.dot(points, line_parameters))
+        gradient = (learning_rate/m)*np.dot(points.T, (p - labels))
+        #print(gradient)
+        line_parameters -= gradient
+        w1 = line_parameters.item(0)
+        w2 = line_parameters.item(1)
+        b = line_parameters.item(2)
+        x1 = np.array([points[:, 0].min(), points[:, 0].max()])
+        x2 = -b/w2 + (x1*(-w1/w2))
+        if x2[0] < 800 and x2[1] < 800:
+            draw(x1, x2)
+        else:
+            print(i, x2)
+
+
 
 np.random.seed(0)
 
@@ -26,37 +47,41 @@ bias = np.ones(n_pts)
 
 random_x1_values = np.random.normal(10, 2, n_pts)
 random_x2_values = np.random.normal(50, 10, n_pts)
-
+#random_x1_values = np.random.normal(10, 2, n_pts)
+#random_x2_values = np.random.normal(12, 2, n_pts)
 top_region = np.array([random_x1_values, random_x2_values, bias]).T
+#bottom_region = np.array([np.random.normal(5, 2, n_pts), np.random.normal(6, 2, n_pts), bias]).T
 bottom_region = np.array([np.random.normal(5, 2, n_pts), np.random.normal(30, 10, n_pts), bias]).T
 all_points = np.vstack((top_region, bottom_region))
 
 # choose a random starting line
-w1 = -0.2
-w2 = -0.35
-b = 3.5
+#w1 = -0.2
+#w2 = -0.35
+#b = 3.5
 
-line_parameters = np.array([w1, w2, b]).reshape(3, 1)
+line_parameters = np.array([np.zeros(3)]).reshape(3, 1)
 
-x1 = np.array([bottom_region[:, 0].min(), top_region[:, 0].max()])
-x2 = -b/w2 - x1*(w1/w2)
+#x1 = np.array([bottom_region[:, 0].min(), top_region[:, 0].max()])
+#x2 = -b/w2 - x1*(w1/w2)
 
-linear_combination = np.dot(all_points, line_parameters)
+#linear_combination = np.dot(all_points, line_parameters)
 #print(linear_combination)
 
-probabilities = sigmoid(linear_combination)
+#probabilities = sigmoid(linear_combination)
 #print(probabilities)
 
-labels = np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2)
-print(calculate_error(line_parameters, all_points, labels))
+labels = np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2, 1)
+#print(calculate_error(line_parameters, all_points, labels))
 
 
 
 
 _, ax = plt.subplots(figsize=(4, 4))
+plt.xlim(0, 15)
+plt.ylim(0, 80)
 ax.scatter(top_region[:, 0], top_region[:, 1], color='r')
 ax.scatter(bottom_region[:, 0], bottom_region[:, 1], color='b')
-draw(x1, x2)
+gradient_descent(line_parameters, all_points, labels, 0.002)
 plt.show()
 
 
